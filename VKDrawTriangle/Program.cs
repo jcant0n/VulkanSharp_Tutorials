f5ad477b095b4f70504b70f85064b43ba1187f0d
Javier Cantón Ferrero <javiercantonferrero@gmail.com>
2018-03-08 20:07:26 +0100

    First commit

---
+++ VKDrawTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace VKDrawTriangle
+{
+    class Program
+    {
+        static Form window;
+        static VulkanRenderer renderer;
+
+        static void Main(string[] args)
+        {
+            window = new Form();
+            window.Text = "VK drawing a triangle";
+            window.Size = new System.Drawing.Size(800, 600);
+            window.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            window.Show();
+
+            renderer = new VulkanRenderer();
+            renderer.InitVulkan(window.Handle, window.Width, window.Height);
+
+            bool isClosing = false;
+            window.FormClosing += (s, e) => 
+            {
+                isClosing = true;
+            };
+
+            while (!isClosing)
+            {
+                renderer.DrawFrame();
+                System.Windows.Forms.Application.DoEvents();
+            }
+
+            renderer.CleanUp();
+        }
+        
+    }
+}
