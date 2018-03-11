1789f65d91b7a128103af02d54d18a67526d5d84
Javier Cantón Ferrero <javiercantonferrero@gmail.com>
2018-03-11 13:27:46 +0100

    Finished VertexBuffers sample.

---
+++ VKVertexBuffers/SwapChainSupportDetails.cs
@@ -0,0 +1,20 @@
+﻿using Vulkan;
+
+namespace VKVertexBuffers
+{
+    public struct SwapChainSupportDetails
+    {
+        public SurfaceCapabilitiesKhr capabilities;
+        public SurfaceFormatKhr[] formats;
+        public PresentModeKhr[] presentModes;
+
+        public bool IsComplete => formats.Length > 0 && presentModes.Length > 0;
+
+        public SwapChainSupportDetails(PhysicalDevice device, SurfaceKhr surface)
+        {
+            capabilities = device.GetSurfaceCapabilitiesKHR(surface);
+            formats = device.GetSurfaceFormatsKHR(surface);
+            presentModes = device.GetSurfacePresentModesKHR(surface);
+        }
+    }
+}
